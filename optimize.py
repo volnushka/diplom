135bcf3d0d16fd4586e0876a176371b3200df817
volnushka <nikel55551@gmail.com>
2021-05-25 11:31:35 +0300

    run block removed

--- optimize.py
+++ optimize.py
@@ -104,22 +104,7 @@ class Population:
         self.offspring[random.randint(0, 4)][random.randint(0, 4)] = random.randint(3, 12)
         # print(self.offspring)
         self.compare_buffer = self.compare[1]
-        p.compare = []
-        p.test(p.offspring)
-        p.compare.insert(0, self.compare_buffer)
+        self.compare = []
+        self.test(self.offspring)
+        self.compare.insert(0, self.compare_buffer)
 
-
-p = Population(8000)
-# p.count_income(p.genes)
-p.test(p.ancestors)
-p.breed(p.ancestors)
-p.test(p.offspring)
-while True:
-    while p.compare[1] < p.compare[0]:  # mutate when offspring is less viable
-        p.mutate()
-    else:  # breed when offspring is more viable
-        p.breed(p.offspring)
-        p.compare_buffer = p.compare[1]
-        p.compare = []
-        p.test(p.offspring)
-        p.compare.insert(0, p.compare_buffer)
