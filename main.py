12b800652bff51a1ac23ef18e090c634fb7d2136
volnushka <49350343+volnushka@users.noreply.github.com>
2022-04-05 10:07:30 +0300

    beautifying

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ operators = {
 }
 
 
-class Population:
+class Population: # class representing a group of species
     def __init__(self, constraint):
         self.total = 0
         self.compare_buffer = 0
@@ -30,31 +30,28 @@ class Population:
         self.offspring = []
         self.compare = []
         self.generation_counter = 0
-        for a in range(5):
+        for a in range(5):  # creating the first pool (5 species each having 4 genes)
             self.ancestors = []
             for i in range(4):
                 self.ancestors.append(random.randint(3, 12))
             self.genes.append(self.ancestors)
-        print('All random integers (genes): ' + str(self.genes))
-
+            
+    # function to count an operator's income
     def count_income(self, random_hours):
         self.total = 0
-        for operator in operators.items():  # 1: (a, 100, [1, 2 ...])
+        for operator in operators.items():
             operator_projects = []
             operator_income = 0
             time = random_hours[operator[0]]
-            for project in project_list.keys():  # 1, 2 ...
+            for project in project_list.keys():
                 if project in operator[1][2]:
                     operator_projects.append(project)
                     operator_income += project_list[project][1] * project_list[project][2]
             final_income = operator_income - operator[1][1] * time
-            # print("{0}\nHours: {1}\n".
-            #       format(op[1][0], time))
             self.total += final_income
-        # print(self.global_income)
-        # print(self.genes)
         return self.total
 
+    # a function to test each generation
     def test(self, genes_list):  # generate 5 specimens out of the equation
         self.specimens = []
         self.coefficients = []
@@ -62,25 +59,21 @@ class Population:
             test_genes = []
             for j in range(4):
                 test_genes.append(genes_list[i][j])
-            # print("Test genes are".format(test_genes))
             self.specimens.append(self.count_income(test_genes))
             self.coefficients.append(abs(int(self.specimens[i]) - self.constraint))
-            if self.coefficients[i] == 0:  # the answer
+            if self.coefficients[i] == 0:  # a condition to finish and get an answer
                 print('Done, the answer is {0}\nSurvivors: {1}'.format(genes_list[i], self.specimens))
                 sys.exit()
             self.reversed_sum += 1 / self.coefficients[i]   # get a sum of reversed coefficients
         print('Generation: {0}\nSpecimens: {1}\n'.format(self.generation_counter, self.specimens))
         x = sum(self.coefficients) / len(self.coefficients)
         self.compare.append(x)
-        # print('Coefficients of viability: ' + str(self.coefficients))
-        # print('Average fitness: ' + str(x))
-        # print('Reversed sum: ' + str(self.reversed_sum))
-
         self.probabilities = []
         for i in range(5):  # probability of each chromosome
             self.probabilities.append((1 / self.coefficients[i]) / self.reversed_sum)
         print('Probabilities: ' + str(self.probabilities))
 
+    #  breeding is initiated whenever n+1 generation is stronger than n generation    
     def breed(self, population):  # generate new offspring
         i = 0
         while i < 5:
@@ -100,10 +93,9 @@ class Population:
         self.generation_counter += 1
         print('Next generation: ' + str(self.offspring))
 
+    # mutation is initiated whenever n+1 generation is weaker than n generation
     def mutate(self):  # change random digit in a random chromosome
-        # print(self.offspring)
         self.offspring[random.randint(0, 4)][random.randint(0, 3)] = random.randint(3, 12)
-        # print(self.offspring)
         self.compare_buffer = self.compare[1]
         p.compare = []
         p.test(p.offspring)
@@ -111,7 +103,6 @@ class Population:
 
 
 p = Population(11000)
-# p.count_income(p.genes)
 p.test(p.genes)
 p.breed(p.genes)
 p.test(p.offspring)
